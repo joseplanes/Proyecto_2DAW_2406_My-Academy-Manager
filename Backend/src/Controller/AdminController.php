434d1838d822e54b1886f1ae4655de3cce8b9caf
Antonio Cano <4144827@alu.murciaeduca.es>
2024-04-13 13:09:28 +0200

    Cambios funciones admin

--- Backend/src/Controller/AdminController.php
+++ Backend/src/Controller/AdminController.php
@@ -46,36 +46,46 @@ class AdminController extends AbstractController
         $authCheck = $jwt_auth->checkToken($token);
 
         if($authCheck){
-            $json = json_decode($request->getContent(), true);
 
             $identity = $jwt_auth->checkToken($token, true);
 
             if($identity->rol == 'admin'){
-
-                $asignatura = new Asignatura();
-                $asignatura->setNombre($json['nombre']);
-                $asignatura->setDescripcion($json['descripcion']);
-                $asignatura->setImg($json['img']);
-                        
-        
-                $entityManager->persist($asignatura);
-                $entityManager->flush();
-
-                $data = [
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Asignatura creada correctamente'
-                ];
-
-            }else{
-                $data = [
-                    'status' => 'error',
-                    'code' => 400,
-                    'message' => 'No tienes permiso para realizar esta acción'
-                ];
-               
+                $json = $request->get('json', null);
+                $params =  json_decode($json);
+                if(!empty($json)){
+                    $nombre= (!empty($params->nombre)) ? $params->nombre : null;
+                    $descripcion= (!empty($params->descripcion)) ? $params->descripcion : null;
+                    $img= (!empty($params->img)) ? $params->img : null;
+                    $asignatura = new Asignatura();
+                    $asignatura->setNombre($nombre);
+                    $asignatura->setDescripcion($descripcion);
+                    $asignatura->setImg($img);
+                    
+                    
+                    $entityManager->persist($asignatura);
+                    $entityManager->flush();
+                    
+                    $data = [
+                        'status' => 'success',
+                        'code' => 200,
+                        'message' => 'Asignatura creada correctamente'
+                    ];
+                }else{
+                    $data = [
+                        'status' => 'error',
+                        'code' => 400,
+                        'message' => 'No se ha podido crear la asignatura'
+                    ];
+                    
+                }
             }
-                
+
+        }else{
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'No tienes permiso para realizar esta acción'
+            ];
             
         }
 
@@ -96,34 +106,44 @@ class AdminController extends AbstractController
         $authCheck = $jwt_auth->checkToken($token);
 
         if($authCheck){
-            $json = json_decode($request->getContent(), true);
-
+            
             $identity = $jwt_auth->checkToken($token, true);
 
             if($identity->rol == 'admin'){
+                $json = $request->get('json', null);
+                $params =  json_decode($json);
 
-                $aula=new Aula();
-                $aula->setNombre($json['nombre']);
-                $aula->setCapacidad($json['capacidad']);
-                        
-        
-                $entityManager->persist($aula);
-                $entityManager->flush();
+                if(!empty($json)){
+                    $nombre= (!empty($params->nombre)) ? $params->nombre : null;
+                    $capacidad= (!empty($params->capacidad)) ? $params->capacidad : null;
+                    $aula = new Aula();
+                    $aula->setNombre($nombre);
+                    $aula->setCapacidad($capacidad);
+                    
+                    $entityManager->persist($aula);
+                    $entityManager->flush();
+                    
+                    $data = [
+                        'status' => 'success',
+                        'code' => 200,
+                        'message' => 'Aula creada correctamente'
+                    ];
 
-
-                $data = [
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Aula creado con éxito'
-                ];
-
+                }else{
+                    $data = [
+                        'status' => 'error',
+                        'code' => 400,
+                        'message' => 'No se ha podido crear el aula'
+                    ];
+                
+                }
             }else{
                 $data = [
                     'status' => 'error',
                     'code' => 400,
                     'message' => 'No tienes permiso para realizar esta acción'
                 ];
-               
+                
             }
                 
             
@@ -133,59 +153,76 @@ class AdminController extends AbstractController
     }
 
 
+
     #[Route('/clase/crear', name: 'crear_clase', methods: ['POST'])]
     public function crearClase(Request $request,JwtAuth $jwt_auth ,SerializerInterface $serializer, EntityManagerInterface $entityManager, AsignaturaRepository $ar, AulaRepository $aulaRepository, DiasSemanaRepository $ds, ProfesorRepository $pr, ClaseRepository $cr)
     {
-        //Recoger token 
+        //Recoger token
         $token = $request->headers->get('Authorization');
-
 
         //Comprobar si es correcto
         $authCheck = $jwt_auth->checkToken($token);
 
         if($authCheck){
-            $json = json_decode($request->getContent(), true);
 
             $identity = $jwt_auth->checkToken($token, true);
 
             if($identity->rol == 'admin'){
-                $clase = new Clase();
-                $clase->setHoraInicio(new \DateTime($json['horaInicio']));
-                $clase->setHoraFin(new \DateTime($json['horaFin']));
+                $json = $request->get('json', null);
+                $params =  json_decode($json);
+                if(!empty($json)){
+                    $hora_inicio= (!empty($params->horaInicio)) ? $params->horaInicio : null;
+                    $hora_fin= (!empty($params->horaFin)) ? $params->horaFin : null;
+                    $asignatura= (!empty($params->asignatura)) ? $params->asignatura : null;
+                    $profesor= (!empty($params->profesor)) ? $params->profesor : null;
+                    $aula= (!empty($params->aula)) ? $params->aula : null;
+                    $dias= (!empty($params->dias)) ? $params->dias : null;
+
+                    $clase=new Clase();
+
+                    $clase->setHoraInicio(new \DateTime($hora_inicio));
+                    $clase->setHoraFin(new \DateTime($hora_fin));
+
+                    $idasignatura = $ar->findOneBy(['id' => $asignatura]);
+                    $idprofesor = $pr->findOneBy(['id' => $profesor]);
+                    $idaula = $aulaRepository->findOneBy(['id' => $aula]);
+
+                    if ($idasignatura && $idprofesor && $idaula) {
+                        
+                        $clase->setAsignatura($idasignatura);
+                        $clase->setProfesor($idprofesor);
+                        $clase->setAula($idaula);
                 
-        
-                $asignatura = $ar->find($json['asignatura']);
-                $profesor = $pr->find($json['profesor']);
-                $aula = $aulaRepository->find($json['aula']);
-        
-                if ($asignatura && $profesor && $aula) {
-                    
-                    $clase->setAsignatura($asignatura);
-                    $clase->setProfesor($profesor);
-                    $clase->setAula($aula);
-            
-                    foreach ($json['dias'] as $diaId) {
-                        $dia = $ds->find($diaId);
-                        if ($dia) {
-                            $clase->addDia($dia);
+                        foreach ($dias as $diaId) {
+                            $dia = $ds->find($diaId);
+                            if ($dia) {
+                                $clase->addDia($dia);
+                            }
                         }
-                    }
-        
-                    $entityManager->persist($clase);
-                    $entityManager->flush();
             
-                    $data = [
-                        'status' => 'success',
-                        'code' => 200,
-                        'message' => 'Clase creada con éxito'
-                    ];
+                        $entityManager->persist($clase);
+                        $entityManager->flush();
+                
+                        $data = [
+                            'status' => 'success',
+                            'code' => 200,
+                            'message' => 'Clase creada con éxito'
+                        ];
+                    }else{
+                        $data = [
+                            'status' => 'error',
+                            'code' => 400,
+                            'message' => 'No se ha podido crear la clase'
+                        ];
+                    
+                    }
                 }else{
                     $data = [
                         'status' => 'error',
                         'code' => 400,
                         'message' => 'No se ha podido crear la clase'
                     ];
-                   
+                
                 }
             }else{
                 $data = [
@@ -193,16 +230,21 @@ class AdminController extends AbstractController
                     'code' => 400,
                     'message' => 'No tienes permiso para realizar esta acción'
                 ];
-               
+            
             }
-
-            return new JsonResponse($data);
-            }
-
+        }else{
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'No tienes permiso para realizar esta acción'
+            ];
         
-       
+        }
 
+        return new JsonResponse($data);
     }
+    
+    
 
     
 }
